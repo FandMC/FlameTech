93abed7fcad063fbfd9488b0bb81a688eb219a19
FandMC <winem2002@outlook.com>
2025-06-08 13:47:35 +0800

    升级0.1.3 修复了部分配方注册失败，简化了矿粉熔炼配方的编写方式，重构为kts[优化][修复bug][重构]

--- Server/src/main/java/cn/fandmc/flametech/recipes/impl/smelting/GoldDustSmeltingRecipe.java
+++ Server/src/main/java/cn/fandmc/flametech/recipes/impl/smelting/GoldDustSmeltingRecipe.java
@@ -4,6 +4,6 @@ import cn.fandmc.flametech.recipes.impl.smelting.base.BaseDustSmeltingRecipe;
 
 public class GoldDustSmeltingRecipe extends BaseDustSmeltingRecipe {
     public GoldDustSmeltingRecipe() {
-        super("gold_dust", "gold_ingot_ft", "smelting_gold_dust", "金粉熔炼", 5);
+        super("gold_dust", "gold_ingot_10", "smelting_gold_dust", "金粉熔炼", 5);
     }
 }
